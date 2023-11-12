923192e771ea28ebd22fc0c5569c8cdff50328dc
Suat Zengin <zenginsuat.inf@gmail.com>
2023-11-12 14:57:12 +0300

    Add fab with circular constraint

--- app/src/main/java/com/suatzengin/iloveanimals/util/extension/ViewExt.kt
+++ app/src/main/java/com/suatzengin/iloveanimals/util/extension/ViewExt.kt
@@ -3,6 +3,12 @@ package com.suatzengin.iloveanimals.util.extension
 import android.app.Activity
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.findViewTreeLifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 fun View.hideKeyboard() {
     val inputMethodManager = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
@@ -12,4 +18,16 @@ fun View.hideKeyboard() {
 fun View.dpAsPixels(size: Int): Int {
     val scale = resources.displayMetrics.density
     return (size * scale + 0.5f).toInt()
+}
+
+fun View.delayOnLifecycle(
+    delayMillis: Long,
+    action: () -> Unit
+) {
+    findViewTreeLifecycleOwner()?.lifecycleScope?.launch {
+        findViewTreeLifecycleOwner()?.repeatOnLifecycle(state = Lifecycle.State.STARTED) {
+            delay(delayMillis)
+            action()
+        }
+    }
 }
